84ecca2c2a025a8cbbf1d5c29e6b0dfd2a7134b6
cienconpoder <cienconsepoder@gmail.com>
2023-06-07 11:55:16 -0500

    add new videos

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import '../styles/globals.css';
 import { AppProps } from 'next/app';
 import { useEffect, useState } from 'react';
 
+
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   const [initialRenderComplete, setInitialRenderComplete] = useState<boolean>(false);
   useEffect(() => {
